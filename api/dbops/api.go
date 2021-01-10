eedce13dd3fd5c6a3e2a2053fb3f3b197dc02fb7
XiaoxiaYao <michaelyaonz@gmail.com>
2021-01-10 15:31:02 +0800

    Handle the request.

---
+++ api/dbops/api.go
@@ -0,0 +1,17 @@
+package dbops
+
+import (
+	"database/sql"
+)
+
+func openConn() *sql.DB {
+
+}
+
+func AddUserCredential(userName string, pwd string) error {
+
+}
+
+func GetUserCredential(userName string) error {
+
+}
